1835317ae25ded27c6d50d9b3a810043b2bcb9ba
sc <mightysam@gmail.com>
2016-08-28 23:12:48 -0400

    initial

---
+++ logosgospelwall/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  logosgospelwall
+//
+//  Created by Sam Cheung on 8/26/16.
+//  Copyright © 2016 Logos. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+public class MainViewController : UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    public override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let destination = segue.destinationViewController as! AVPlayerViewController
+        
+        if let url = NSBundle.mainBundle().pathForResource("Lego", ofType: "mp4") {
+            //destination.player = AVPlayer(URL: url)
+        }else  {
+            print("No URL")
+        }
+    }
+}
---
+++ logosgospelwall/VideoPlayerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  VideoPlayerViewController.swift
+//  logosgospelwall
+//
+//  Created by Sam Cheung on 8/27/16.
+//  Copyright © 2016 Logos. All rights reserved.
+//
+
+import AVKit
+import AVFoundation
+
+public class VideoPlayerViewController : AVPlayerViewController {
+    
+}
